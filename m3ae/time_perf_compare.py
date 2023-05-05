0dbd3b0850b48cece9654ba54e4026beefd67391
levi-kennedy <levi@kennedykin.com>
2023-05-05 02:47:41 +0000

    Updated parameters

--- m3ae/time_perf_compare.py
+++ m3ae/time_perf_compare.py
@@ -7,7 +7,7 @@ def cpu():
 
     startTime = time.time()
     with tf.device('/cpu:0'):
-        random_matrix_cpu = tf.random.uniform(shape=(10000, 10000), minval=0, maxval=1)
+        random_matrix_cpu = tf.random.uniform(shape=(30000, 30000), minval=0, maxval=1)
         dot_operation_cpu = tf.matmul(random_matrix_cpu, tf.transpose(random_matrix_cpu))
         sum_operation_cpu = tf.reduce_sum(dot_operation_cpu)
 
@@ -23,7 +23,7 @@ def gpu():
 
     startTime = time.time()
     with tf.device('/gpu:0'):
-        random_matrix_gpu = tf.random.uniform(shape=(10000, 10000), minval=0, maxval=1)
+        random_matrix_gpu = tf.random.uniform(shape=(30000, 30000), minval=0, maxval=1)
         dot_operation_gpu = tf.matmul(random_matrix_gpu, tf.transpose(random_matrix_gpu))
         sum_operation_gpu = tf.reduce_sum(dot_operation_gpu)
 
@@ -38,4 +38,5 @@ def gpu():
 if __name__ == "__main__":
     cpu_time = cpu()
     gpu_time = gpu()
-    print("GPU is {}x faster than CPU".format(int(cpu_time/gpu_time)))
+    # print the cpu/gpu fraction to the first decimal place
+    print(f'GPU is {cpu_time/gpu_time:.1f}x faster than CPU')
