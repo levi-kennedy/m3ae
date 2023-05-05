318028b08fc38a049c9f11466975338fe1d77093
levi-kennedy <levi@kennedykin.com>
2023-05-05 02:14:10 +0000

    Updated params

--- m3ae/time_perf_compare.py
+++ m3ae/time_perf_compare.py
@@ -4,15 +4,14 @@ import tensorflow as tf
 
 # Create a function to perform a tensor multiplication using the CPU and another function to perform the same operation using the GPU and compare the relative times of execution between the two.
 def cpu():
+
+    startTime = time.time()
     with tf.device('/cpu:0'):
-        random_matrix_cpu = tf.random.uniform(shape=(1000, 1000), minval=0, maxval=1)
+        random_matrix_cpu = tf.random.uniform(shape=(10000, 10000), minval=0, maxval=1)
         dot_operation_cpu = tf.matmul(random_matrix_cpu, tf.transpose(random_matrix_cpu))
         sum_operation_cpu = tf.reduce_sum(dot_operation_cpu)
 
-    startTime = time.time()
-    with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) as session:
-        result = session.run(sum_operation_cpu)
-        print(result)
+    
     time_diff = time.time() - startTime
     print("\n" * 2)
     print("CPU Time taken:", time_diff)
@@ -21,15 +20,14 @@ def cpu():
     return time_diff
 
 def gpu():
+
+    startTime = time.time()
     with tf.device('/gpu:0'):
-        random_matrix_gpu = tf.random.uniform(shape=(1000, 1000), minval=0, maxval=1)
+        random_matrix_gpu = tf.random.uniform(shape=(10000, 10000), minval=0, maxval=1)
         dot_operation_gpu = tf.matmul(random_matrix_gpu, tf.transpose(random_matrix_gpu))
         sum_operation_gpu = tf.reduce_sum(dot_operation_gpu)
 
-    startTime = time.time()
-    with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) as session:
-        result = session.run(sum_operation_gpu)
-        print(result)
+    
     time_diff = time.time() - startTime
     print("\n" * 2)
     print("GPU Time taken:", time_diff)
